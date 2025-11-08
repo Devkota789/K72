2070100fb01410df0ff25984ef6abe7ce8b26ac3
Rdinesh1667 <your-email@example.com>
2025-11-08 20:08:31 +0545

    first commit

---
+++ src/components/home/Video.jsx
@@ -0,0 +1,19 @@
+// src/components/home/Video.jsx
+import React from 'react';
+
+const Video = () => {
+  return (
+    <div className='h-full w-full'>
+      <video
+        className='h-full w-full object-cover'
+        autoPlay
+        loop
+        muted
+       src="/vidoooo.mp4"
+      />
+    </div>
+  )
+}
+
+export default Video;
+
